50bf6c2df1954dca552055b18654a0cb381fc5f7
Suryansh Gupta <guptajisuryansh286@gmail.com>
2026-03-21 19:03:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -422,3 +422,4 @@ async def verify_news(headline: str = Form(...)):
 # if __name__ == "__main__":
 #     port = int(os.environ.get("PORT", 10000))
 #     uvicorn.run(app, host="0.0.0.0", port=port)
+app.include_router(news_router)
